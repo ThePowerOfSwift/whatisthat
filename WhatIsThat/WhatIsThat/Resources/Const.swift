404a122835caeb87b7484a916fb2f43911dca56e
koji watanabe <dubianhao2@gmail.com>
2016-10-16 14:32:01 +0900

    Add corporate logo.

---
+++ WhatIsThat/WhatIsThat/Resources/Const.swift
@@ -0,0 +1,19 @@
+//
+//  Const.swift
+//  WhatIsThat
+//
+//  Created by 渡邊浩二 on 2016/10/16.
+//  Copyright © 2016年 渡邊浩二. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Const {
+    struct Color {
+        static let CorporateLogo = UIColor.black
+    }
+    struct Screen {
+        static let Size = UIScreen.main.bounds.size
+    }
+}
