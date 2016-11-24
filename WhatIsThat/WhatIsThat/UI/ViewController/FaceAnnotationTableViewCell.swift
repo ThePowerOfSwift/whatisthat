176be3510ac8038924362ee1963fa85f3882b058
渡邊浩二 <dubianhao2@gmail.com>
2016-11-25 02:17:55 +0900

    Fix bug of face annotations.

--- WhatIsThat/WhatIsThat/UI/ViewController/FaceAnnotationTableViewCell.swift
+++ WhatIsThat/WhatIsThat/UI/ViewController/FaceAnnotationTableViewCell.swift
@@ -14,13 +14,32 @@ class FaceAnnotationTableViewCell: UITableViewCell {
     @IBOutlet weak var sorrowLikelihoodLabel: UILabel!
     @IBOutlet weak var angerLikelihoodLabel: UILabel!
     
-    var result: FaceAnnotation?
+    var result: FaceAnnotation? {
+        didSet {
+            setContent()
+        }
+    }
     
     func setContent() {
         guard let result = result else { return }
-        joyLikelihoodLabel.text      = String(result.joyLikelihood)
-        surpriseLikelihoodLabel.text = String(result.surpriseLikelihood)
-        sorrowLikelihoodLabel.text   = String(result.sorrowLikelihood)
-        angerLikelihoodLabel.text    = String(result.angerLikelihood)
+        joyLikelihoodLabel.text      = getExpressionScore(likelihood: result.joyLikelihood) + "%"
+        surpriseLikelihoodLabel.text = getExpressionScore(likelihood: result.surpriseLikelihood) + "%"
+        sorrowLikelihoodLabel.text   = getExpressionScore(likelihood: result.sorrowLikelihood) + "%"
+        angerLikelihoodLabel.text    = getExpressionScore(likelihood: result.angerLikelihood) + "%"
+    }
+    
+    private func getExpressionScore(likelihood: String) -> String {
+        if Likelihood.VeryLikely.rawValue == likelihood {
+            return "100"
+        } else if Likelihood.Likely.rawValue == likelihood {
+            return "80"
+        } else if Likelihood.Possible.rawValue == likelihood {
+            return "60"
+        } else if Likelihood.Unlikely.rawValue == likelihood {
+            return "20"
+        } else if Likelihood.VeryUnlikely.rawValue == likelihood {
+            return "0"
+        }
+        return "40"
     }
 }
